391cf417c35de4ec4a7f8021b265afafce41428f
chrispobrien <chris.obrien57@gmail.com>
2022-08-17 13:34:47 -0400

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,54 @@
+const APP_PREFIX = 'Radium-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js"
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache: ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeeplist = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('fetch request: ' + event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache: ' + event.request.url);
+                return request;
+            } else { // if not found in cache, try fetching request
+                console.log('file is not cached, fetching: ' + event.request.url);
+                return fetch(event.request);
+            };
+        })
+    );
+});
