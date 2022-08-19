b850322a99736653ca596faa2102fda69502544e
chrispobrien <chris.obrien57@gmail.com>
2022-08-18 22:01:06 -0400

    Some work on readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# 19-budget-radium
-Budget Tracker App
+# Radium Budget Tracker
+
+## Description
+
+This budget tracker app allows you to record your expenses and income while online or offline! It is a progressive web app (PWA) and you can install it on your cell phone, to your home screen, or on your desktop or laptop!
+
--- public/js/idb.js
+++ public/js/idb.js
@@ -7,7 +7,7 @@ request.onupgradeneeded = function(event) {
     // save a reference to the database
     const db = event.target.result;
     // create an object store (table) called 'new_item', set it to have an auto incrementing primary key of sorts
-    db.createObjectStore('new_item', {autoIncrement: true} );
+    db.createObjectStore('item', {autoIncrement: true} );
 };
 
 // upon a successful
@@ -19,6 +19,8 @@ request.onsuccess = function(event) {
     if (navigator.online) {
         // upload any items created offline
         uploadItem();
+        // create a local copy
+        downloadItem();
     }
 };
 
@@ -30,22 +32,22 @@ request.onerror = function(event) {
 // This function will be executed if we attempt to submit a new item and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_item'], 'readwrite');
+    const transaction = db.transaction(['item'], 'readwrite');
 
     // access IndexedDB local object store
-    const itemObjectStore = transaction.objectStore('new_item');
+    const itemObjectStore = transaction.objectStore('item');
 
     // add record to IndexedDB local object store with add method
     itemObjectStore.add(record);
 };
 
-// send local IndexedDb items to server
+// clone server items - send local IndexedDb items to server and vice versa
 function uploadItem() {
     // open a transaction on your db
-    const transaction = db.transaction(['new_item'], 'readwrite');
+    const transaction = db.transaction(['item'], 'readwrite');
 
     // access your object store
-    const itemObjectStore = transaction.objectStore('new_item');
+    const itemObjectStore = transaction.objectStore('item');
 
     // get all records from store and set to a variable
     const getAll = itemObjectStore.getAll();
@@ -68,9 +70,9 @@ function uploadItem() {
                     throw new Error(serverResponse);
                 }
                 // open one more transaction
-                const transaction = db.transaction(['new_item'], 'readwrite');
-                // access the new_item object store
-                const itemObjectStore = transaction.objectStore('new_item');
+                const transaction = db.transaction(['item'], 'readwrite');
+                // access the local item object store
+                const itemObjectStore = transaction.objectStore('item');
                 // clear all items in your store
                 itemObjectStore.clear();
 
